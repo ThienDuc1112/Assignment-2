f22136312c989f2bd4eee8fd9be4b279cbbde388
ThienDuc1112 <95234772+ThienDuc1112@users.noreply.github.com>
2021-12-31 09:48:05 +0700

    Add files via upload

---
+++ Hotel.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASS2
+{
+    public class Room
+    {
+        public int NameRoom { get; set; }
+        public string TypeRoom { get; set; }
+        public int PriceRoom { get; set; }
+        public string StatusRoom { get; set; }
+        public Room()
+        {
+
+        }     
+
+        public void InputRoom()
+        {
+            Console.WriteLine("Enter the name of the room:");
+            NameRoom = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter type of room: ");
+            TypeRoom = Console.ReadLine();
+            Console.WriteLine("Enter price of the room: ");
+            PriceRoom = int.Parse(Console.ReadLine());
+        }
+      
+        public void DisplayRoom()
+        {
+            Console.WriteLine("______________________________________________________________________");
+            Console.WriteLine("---------------------Information Room---------------------------------");
+            Console.WriteLine($"Name room: {NameRoom}--Type room: {TypeRoom}--Price: {PriceRoom}$--Status: {StatusRoom}");
+        }
+
+    }
+    public class CustomerInRoom : Room
+    {
+
+        public string FullName { get; set; }
+        public int PhoneNumber { get; set; }
+        public int IDNumber { get; set; }
+       
+       
+        public CustomerInRoom()
+        {
+
+        }
+        public void NullInformation()
+        {
+            FullName = " ";
+            PhoneNumber = 0;
+            IDNumber = 0;
+            StatusRoom = "UnBooked";
+        }
+        public void InputCustomer()
+        {
+            Console.WriteLine("Enter full name of a customer: ");
+            FullName = Console.ReadLine();
+            Console.WriteLine("Enter phone number of a customer: ");
+            PhoneNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter ID number of customer: ");
+            IDNumber = int.Parse(Console.ReadLine());
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("---------------------Information Customer--------------------------");
+            Console.WriteLine($"Full name of customer: {FullName}");
+            Console.WriteLine($"Phone number of customer: {PhoneNumber}");
+            Console.WriteLine($"ID number of customer: {IDNumber}");
+            Console.WriteLine("______________________________________________________________________");
+        }
+
+
+    }
+    public class Processor
+    {
+        public static List<Room> Rooms { get; set; }
+        public static List<CustomerInRoom> Customers { get; set; }
+        public static int NumberRoom { get; set; }
+
+        public static void PrintRooms()
+        {
+            foreach (CustomerInRoom customer in Customers)
+            {
+                customer.DisplayRoom();
+                customer.Display();
+            }
+        }
+        public static void Delete()
+        {
+            Console.WriteLine("Do you really want to delete the room");
+            Console.WriteLine("Please enter yes or no ");
+            string option = Console.ReadLine();
+            if (option == "yes")
+            {
+                Console.WriteLine("Enter the name room need to delete: ");
+                int nameRoom = int.Parse(Console.ReadLine());
+                for (int i = 0; i < NumberRoom; i++)
+                {
+                    if (Customers[i].NameRoom.Equals(nameRoom))
+                    {
+                        Customers.RemoveAll(x => x.NameRoom == nameRoom);
+                    }
+                }
+                NumberRoom--;
+            }
+            if (option == "no")
+            {
+                Console.WriteLine("You entered no, the hotel hasn't any change");
+            }
+        }
+
+        public static void Update()
+        {
+            int option;
+            Console.WriteLine("Enter name of the room need to update:");
+            int nameRoom = int.Parse(Console.ReadLine());
+            for (int i = 0; i < NumberRoom; i++)
+            {
+                if (Customers[i].NameRoom.Equals(nameRoom))
+                {
+                    do
+                    {
+                        Console.WriteLine("Enter 1 to update information of the room");
+                        Console.WriteLine("Enter 2 to update information of the customer");
+                        Console.WriteLine("Enter 3 to exit");
+                        option = int.Parse(Console.ReadLine());
+                        if (option == 1)
+                        {
+                            Customers[i].InputRoom();
+                        }
+                        if (option == 2)
+                        {
+                            Customers[i].InputCustomer();
+                        }
+                        if(option != 1 && option != 2 && option != 3)
+                        {
+                            Console.WriteLine("Please input from 1 to 3");
+                        }
+                    } while (option != 3);
+                }
+            }
+        }
+        public static void Booking()
+        {
+            int nameRoom = 0;
+            Console.WriteLine("Choose a room in the hotel: ");
+            nameRoom = int.Parse(Console.ReadLine());
+            for (int i = 0; i < NumberRoom; i++)
+            {
+                if (Customers[i].NameRoom.Equals(nameRoom))
+                {
+                    if (Customers[i].StatusRoom == "UnBooked")
+                    {                     
+                                Customers[i].InputCustomer();
+                                Customers[i].StatusRoom = "Booked";
+                                break;
+                    }                                         
+                    else
+                    {
+                        Console.WriteLine("Room is booked, please book other room");
+                    }
+                }
+            }
+        }
+        public static void CalculatingBill()
+        {
+            int num;
+            int bill = 0;
+            Console.WriteLine("Enter the number of rooms that are rented: ");
+            num = int.Parse(Console.ReadLine());
+            Checking(num);
+            for (int j = 0; j < num; j++)
+            {             
+                Console.WriteLine("Choose a room in the hotel: ");
+                int nameRoom = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the number of days rented: ");
+                int day = int.Parse(Console.ReadLine());
+                for (int i = 0; i < NumberRoom; i++)
+                {
+                    if (Customers[i].NameRoom.Equals(nameRoom))
+                    {
+                        bill = Customers[i].PriceRoom * day + bill;
+                        Customers[i].NullInformation();
+                    }
+                }
+
+            }
+            Console.WriteLine($"The total money must pay is: { bill}");
+        }
+
+        static void Checking(int num)
+        {
+            while(num > NumberRoom)
+            {
+                Console.WriteLine("Wrong input, import again!!");
+                Console.WriteLine("Enter the number of rooms that are rented: ");
+                num = int.Parse(Console.ReadLine());
+               
+            } 
+            
+            
+        }
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASS2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            do
+            {
+             
+            Console.WriteLine("\t\t\t_______________________Menu________________________________");
+            Console.WriteLine("\t\t\t| Choice 1: Setup the rooms in the hotel                   |");
+            Console.WriteLine("\t\t\t| Choice 2: Watch the room in the hotel                    |");
+            Console.WriteLine("\t\t\t| Choice 3: Book a room in the hotel                       |");
+            Console.WriteLine("\t\t\t| Choice 4: Calculate the bill for customers               |");
+            Console.WriteLine("\t\t\t| Choice 5: Exit the program                               |");
+            Console.WriteLine("\t\t\t|__________________________________________________________|");
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        SetUp();                       
+                        break;
+                    case 2:
+                        Processor.PrintRooms();
+                        break;
+                    case 3:
+                        Processor.Booking();
+                        break;
+                    case 4:
+                        Processor.CalculatingBill();
+                        break;
+                    case 5:
+                        Console.WriteLine("Exit the program successfully!!!");
+                        break;
+                    default:
+                        Console.WriteLine("Wrong input, please enter from 1 to 5");
+                        break;
+                }
+            } while (choice != 5);
+            Console.ReadKey();
+        }
+             static void SetUp()
+            {
+                int password, choice;
+                do
+                {
+                    Console.WriteLine("Please enter password");
+                    password = int.Parse(Console.ReadLine());
+                    if (password == 123456)
+                    {
+                        Console.WriteLine("Login successfull");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong password, please input again");
+                    }
+                } while (password != 123456);
+            do
+            {
+                Console.WriteLine("\t\t\t_________________________SetUp____________________________");
+                Console.WriteLine("\t\t\t| Choice 1: Creating rooms in the hotel                   |");
+                Console.WriteLine("\t\t\t| Choice 2: Creating an extra room                        |");
+                Console.WriteLine("\t\t\t| Choice 3: Update information for the hotel              |");
+                Console.WriteLine("\t\t\t| Choice 4: Delate a room in the hotel                    |");
+                Console.WriteLine("\t\t\t| Choice 5: View rooms in the hotel                       |");
+                Console.WriteLine("\t\t\t| Choice 6: Exit the setup mode                           |");
+                Console.WriteLine("\t\t\t|_________________________________________________________|");
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        InputRooms();
+                        break;
+                    case 2:
+                        CreateARoom();
+                        break;
+                    case 3:
+                        Processor.Update();
+                        break;
+                    case 4:
+                        Processor.Delete();
+                        break;
+                    case 5:
+                        Processor.PrintRooms();
+                        break;
+                    case 6:
+                        Console.WriteLine("Exit the mode setup successfully!!!");
+                        break;
+                    default:
+                        Console.WriteLine("Wrong input, please enter from 1 to 6");
+                        break;
+                }
+            } while (choice != 6);
+        }
+        static void InputRooms()
+        {
+          
+            List<CustomerInRoom> customers = new List<CustomerInRoom>();
+            Console.WriteLine("Enter number of room in the hotel: ");
+            int number = int.Parse(Console.ReadLine());
+            for(int i = 0; i<number; i++)
+            {            
+                CustomerInRoom customer = new CustomerInRoom();                                
+                Console.WriteLine($"Enter the name of the room {i + 1}:");
+                int nameRoom = int.Parse(Console.ReadLine());
+                customer.NameRoom = nameRoom;                                 
+                Console.WriteLine("Enter type of room: ");
+                string typeRoom = Console.ReadLine();
+                customer.TypeRoom = typeRoom;
+                Console.WriteLine("Enter price of the room: ");
+                int price = int.Parse(Console.ReadLine());
+                customer.PriceRoom = price;
+                customer.NullInformation();
+                customers.Add(customer);
+               
+            }
+            Processor.Customers = customers;
+            Processor.NumberRoom = number;
+         
+        }
+        static void CreateARoom()
+        {
+            CustomerInRoom customer2 = new CustomerInRoom();
+            Console.WriteLine("Enter the name of the room:");
+            int nameRoom = int.Parse(Console.ReadLine());
+            customer2.NameRoom = nameRoom;
+            Console.WriteLine("Enter type of room: ");
+            string typeRoom = Console.ReadLine();
+            customer2.TypeRoom = typeRoom;
+            Console.WriteLine("Enter price of the room: ");
+            int price = int.Parse(Console.ReadLine());
+            customer2.PriceRoom = price;
+            customer2.NullInformation();
+            Processor.Customers.Add(customer2);
+            Processor.NumberRoom++;
+        }
+       
+    }
+}
+
+
